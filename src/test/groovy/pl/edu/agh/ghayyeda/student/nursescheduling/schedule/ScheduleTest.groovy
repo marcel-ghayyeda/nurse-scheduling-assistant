1961457eae09941fa1e3a16c34947578550ea333
Marcel Ghayyeda <marcel.ghayyeda@gmail.com>
2019-03-16 09:19:06 +0100

    Test Schedule's equals and hashcode contract

--- src/test/groovy/pl/edu/agh/ghayyeda/student/nursescheduling/schedule/ScheduleTest.groovy
+++ src/test/groovy/pl/edu/agh/ghayyeda/student/nursescheduling/schedule/ScheduleTest.groovy
@@ -10,6 +10,43 @@ import static pl.edu.agh.ghayyeda.student.nursescheduling.schedule.Shift.*
 
 class ScheduleTest extends Specification {
 
+    def "Should correctly compare schedules using equals and hashcode"() {
+        given:
+        def schedule1 = schedule()
+                .forMonth(NOVEMBER)
+                .forYear(2018)
+                .onDay(1, employeeShiftAssignment().employee(Employee.nurse("Nurse 1")).shift(MORNING))
+                .onDay(1, employeeShiftAssignment().employee(Employee.nurse("Nurse 2")).shift(DAY))
+                .onDay(2, employeeShiftAssignment().employee(Employee.nurse("Nurse 1")).shift(DAY))
+                .onDay(2, employeeShiftAssignment().employee(Employee.nurse("Nurse 2")).shift(NIGHT))
+                .build()
+
+        def schedule1Copy = schedule()
+                .forMonth(NOVEMBER)
+                .forYear(2018)
+                .onDay(1, employeeShiftAssignment().employee(Employee.nurse("Nurse 1")).shift(MORNING))
+                .onDay(1, employeeShiftAssignment().employee(Employee.nurse("Nurse 2")).shift(DAY))
+                .onDay(2, employeeShiftAssignment().employee(Employee.nurse("Nurse 1")).shift(DAY))
+                .onDay(2, employeeShiftAssignment().employee(Employee.nurse("Nurse 2")).shift(NIGHT))
+                .build()
+
+        def anotherSchedule = schedule()
+                .forMonth(NOVEMBER)
+                .forYear(2018)
+                .onDay(1, employeeShiftAssignment().employee(Employee.nurse("Nurse 1")).shift(DAY))
+                .onDay(1, employeeShiftAssignment().employee(Employee.nurse("Nurse 2")).shift(NIGHT))
+                .onDay(2, employeeShiftAssignment().employee(Employee.nurse("Nurse 1")).shift(MORNING))
+                .onDay(2, employeeShiftAssignment().employee(Employee.nurse("Nurse 2")).shift(DAY))
+                .build()
+
+        expect:
+        schedule1.hashCode() == schedule1Copy.hashCode()
+        schedule1.equals(schedule1Copy)
+
+        schedule1.hashCode() != anotherSchedule.hashCode()
+        !schedule1.equals(anotherSchedule)
+    }
+
     def "Should create new schedules by adding exactly one working shift in place of free shift"() {
         given:
         def originalSchedule = baseScheduleBuilderWithAllWorkingShifts()

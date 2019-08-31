d92c346a1e893f61cb552ec415272863d3364e8f
Marcel Ghayyeda <marcel.ghayyeda@gmail.com>
2019-08-31 18:31:20 +0200

    Add nurse image on home page

--- src/main/java/pl/edu/agh/ghayyeda/student/nursescheduling/view/HomeLayout.java
+++ src/main/java/pl/edu/agh/ghayyeda/student/nursescheduling/view/HomeLayout.java
@@ -1,12 +1,24 @@
 package pl.edu.agh.ghayyeda.student.nursescheduling.view;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+import static pl.edu.agh.ghayyeda.student.nursescheduling.view.util.ComponentUtil.centered;
+
 @Route(value = "", layout = MainLayout.class)
+@StyleSheet("frontend://css/main.css")
 public class HomeLayout extends VerticalLayout {
     public HomeLayout() {
-        add(new Label("Home Page"));
+        Image nurseImage = new Image("frontend/nurse.png", "Pretty nurse");
+        nurseImage.setHeight("700px");
+        Label welcomeMessage = new Label("Welcome to the Nurse Scheduling Assistant!");
+        welcomeMessage.addClassName("welcome-message");
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.add(centered(nurseImage));
+        verticalLayout.add(centered(welcomeMessage));
+        add(verticalLayout);
     }
 }
